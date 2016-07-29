26a372ce50134332d6bb0ec5920edc9485b328dc
Shingo Tomioka <s.tomioka.103@gmail.com>
2016-07-29 16:32:03 +0900

    Add Alignment class

---
+++ alignment.py
@@ -0,0 +1,12 @@
+class Alignment(object):
+    def __init__(self):
+        self.q_name = ''
+        self.q_bgn  = 0
+        self.q_end  = 0
+        self.q_len  = 0
+        self.t_name = ''
+        self.t_bgn  = 0
+        self.t_end  = 0
+        self.t_len  = 0
+        self.aln_len = 0
+        self.aln_identity = 0.0
