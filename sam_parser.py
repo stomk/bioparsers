beec9ecf02c6949eddb33a4bd2480f0653fc6ccd
Shingo Tomioka <tomioka_shingo_15@cbms.k.u-tokyo.ac.jp>
2016-11-16 12:12:08 +0900

    Use parse instead of parse_sam

--- sam_parser.py
+++ sam_parser.py
@@ -1,6 +1,6 @@
 from sam_alignment import SamAlignment
 
-def parse_sam(sam_file):
+def parse(sam_file):
     with open(sam_file, 'r') as f:
         for line in f:
             yield SamAlignment(line)
